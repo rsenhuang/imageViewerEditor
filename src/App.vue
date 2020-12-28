b07cedb98a92cd700492315204eb2a46ccd08633
rsenhuang <32640697+rsenhuang@users.noreply.github.com>
2020-12-28 18:07:36 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="img_container">
+      <div v-for="(url, index) in imgList" :key="index">
+        <img v-if="index=== active" :src="url" />
+      </div>
+      <div id="image"></div>
+    </div>
+    <Previewer :imgs="imgList" />
+  </div>
+</template>
+
+<script>
+import "viewerjs/dist/viewer.css";
+import Viewer from "viewerjs";
+import Previewer from "@/components/Previewer";
+export default {
+  name: "App",
+  components: {
+    Previewer
+  },
+  data() {
+    return {
+      imgList: [
+        "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1612138888,1794405442&fm=26&gp=0.jpg",
+        "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1588620919,359805583&fm=26&gp=0.jpg",
+        "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1652123795,1945063222&fm=26&gp=0.jpg"
+      ],
+      active: 0
+    };
+  },
+  mounted() {
+    const viewer = new Viewer(document.getElementById("image"), {
+      inline: true,
+      viewed() {
+        viewer.zoomTo(1);
+      }
+    });
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.img_container {
+  display: inline-block;
+  padding: 10px;
+  margin-right: 10px;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  width: 500px;
+  vertical-align: top;
+  img {
+    width: 500px;
+  }
+}
+</style>
---
+++ src/components/Previewer.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="canvas_container">
+    <canvas id="canvas" width="500" height="500"></canvas>
+    <div class="acts">
+        <div class="act_btn">逆时针旋转</div>
+        <div class="act_btn">顺时针旋转</div>
+        <div class="act_btn">放大</div>
+        <div class="act_btn">缩小</div>
+        <div class="act_btn">裁剪</div>
+        <div class="act_btn">涂鸦</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Previewer",
+  props: {
+    imgs: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  mounted() {
+    const c = document.getElementById("canvas");
+    const cxt = c.getContext("2d");
+    let image = new Image();
+    image.src = this.imgs[0]
+    cxt.drawImage(image, 0, 0);
+  },
+  methods: {
+    handleRotate(direction = ''){
+console.log('direction', direction)
+    },
+    handleScale(size = '') {
+      console.log(size)
+    },
+  }
+};
+</script>
+
+<style lang="scss">
+.canvas_container {
+  border: 1px solid #ddd;
+  padding: 10px;
+  width: 1000px;
+  display: inline-block;
+}
+.acts{
+display: flex;
+flex-direction: row;
+align-items: center;
+}
+.act_btn{
+    margin-right: 20px;
+    border: 1px solid #ddd;
+width: 90px;
+text-align: center;
+border-radius: 5px;
+color: #333;
+padding: 10px 0;
+&:hover{
+    color: white;
+    background: chocolate;
+    border: 1px  solid chocolate;
+}
+}
+</style>
