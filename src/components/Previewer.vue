2ef38d33064b353e883dc88827290feaea7e751c
rsenhuang <32640697+rsenhuang@users.noreply.github.com>
2020-12-29 17:48:18 +0800

    Add files via upload

--- src/components/Previewer.vue
+++ src/components/Previewer.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="canvas_container">
-    <canvas id="canvas" width="500" height="500"></canvas>
+    <canvas id="canvas" width="500" height="400" style="border: 1px dashed #ddd;"></canvas>
+    <canvas id="sketch" :width="sketchWidth" :height="sketchHeight" style="border: 1px dashed #ddd;"></canvas>
     <div class="acts">
-        <div class="act_btn">逆时针旋转</div>
-        <div class="act_btn">顺时针旋转</div>
-        <div class="act_btn">放大</div>
-        <div class="act_btn">缩小</div>
+        <div class="act_btn" @click="handleRotate(false)">逆时针旋转</div>
+        <div class="act_btn" @click="handleRotate(true)">顺时针旋转</div>
+        <div class="act_btn" @click="handleScale(true)">放大</div>
+        <div class="act_btn" @click="handleScale(false)">缩小</div>
         <div class="act_btn">裁剪</div>
         <div class="act_btn">涂鸦</div>
+        <div class="act_btn" @click="reset">还原</div>
+        <div class="act_btn" @click="canvasToUrl">保存</div>
+    </div>
+    <div>
+      <div>效果图</div>
+      <img v-if="endImage" :src="endImage" alt="" height="300" />
     </div>
   </div>
 </template>
 
 <script>
+let cxt = undefined;
+const scopeX = 200;
+const scopeY = 200;
 export default {
   name: "Previewer",
   props: {
@@ -23,20 +33,119 @@ export default {
       }
     }
   },
+  data(){
+    return {
+      center: {
+        x: 0,
+        y: 0,
+      },
+      endImage: '',
+      sketchWidth: 0, // 效果
+      sketchHeight: 0, // 效果
+      direction: 1, // 旋转次数
+      scale: 1 // 缩放比例
+    }
+  },
+  created(){
+    console.log('this', this, this.center)
+  },
   mounted() {
+    console.log('this', this, this.center.x)
     const c = document.getElementById("canvas");
-    const cxt = c.getContext("2d");
+    cxt = c.getContext("2d");  
+    this.center.x = 250;
+    this.center.y = 250;
+    cxt.lineWidth = 1;
+    // cxt.strokeRect(0.5, 0.5, 200, 200);  
+    cxt.translate(this.center.x, this.center.y)
     let image = new Image();
-    image.src = this.imgs[0]
-    cxt.drawImage(image, 0, 0);
+    image.setAttribute("crossOrigin",'anonymous')
+    image.src = this.imgs[0];
+    let img = this.computedSize(image);
+    this.sketchWidth = img.width;
+    this.sketchHeight = img.height;
+    cxt.drawImage(image, -img.width / 2, -img.height / 2, img.width, img.height);  
   },
   methods: {
-    handleRotate(direction = ''){
-console.log('direction', direction)
+    // 计算宽高
+    computedSize(image = {}){
+      console.log('计算', image.width, image.height)
+      let height = 200,
+      width = 200;
+      if (image.width > image.height) {
+        height = width * image.height / image.width;
+      } else {
+        width = height * image.width / image.height;
+      }
+      return {
+        height,
+        width
+      }
     },
-    handleScale(size = '') {
-      console.log(size)
+    handleRotate(direction){ 
+      if (this.direction < 4){
+        this.direction += 1;
+      } else {
+        this.direction = 1;
+      }
+      cxt.clearRect(-scopeX, -scopeY, 2 * scopeX, 2 * scopeY)
+      cxt.rotate((direction ? 90 : -90)*Math.PI/180)
+      // setTimeout(() => { 
+      let image = new Image();
+      image.src = this.imgs[0];
+      image.setAttribute("crossOrigin",'anonymous');
+      let img = this.computedSize(image);
+      this.sketchWidth = (this.direction % 2 ? img.width : img.height) * this.scale;
+      this.sketchHeight = (this.direction % 2 ? img.height : img.width) * this.scale;
+      console.log('宽高', img.width, img.height, this.sketchWidth, this.sketchHeight)
+      cxt.drawImage(image, -img.width / 2, -img.height / 2, img.width, img.height);
+      // }, 1000)
     },
+    handleScale(big = true) { 
+      let size = 1 + (big ? 0.2 : -0.2)
+      cxt.clearRect(-scopeX, -scopeY, 2 * scopeX, 2 * scopeY)
+      cxt.scale(size, size)
+      let image = new Image();
+      image.src = this.imgs[0];
+      image.setAttribute("crossOrigin",'anonymous');
+      let img = this.computedSize(image);
+      this.scale += (big ? 0.2 : -0.2);
+      this.sketchWidth = img.width * this.scale;
+      this.sketchHeight = img.height * this.scale;
+      cxt.drawImage(image, -img.width / 2, -img.height / 2, img.width, img.height);
+    }, 
+    reset(){ 
+      // 重置宽高
+      this.sketchWidth = 0;
+      this.sketchHeight = 0;
+      this.scale = 1;
+      this.direction = 1;
+      cxt.lineWidth = 1;
+      cxt.clearRect(-scopeX, -scopeY, 2 * scopeX, 2 * scopeY)
+      // cxt.translate(this.center.x, this.center.y)
+      setTimeout(() => { 
+      let image = new Image();
+      image.setAttribute("crossOrigin",'anonymous')
+      image.src = this.imgs[0];
+      let img = this.computedSize(image);
+      console.log('reset', img.width, img.height)
+      // cxt.drawImage(image, -img.width / 2, -img.height / 2, img.width, img.height); 
+      cxt.drawImage(image, -img.width / 2, -img.height / 2, 112, 200); 
+      }, 1000)
+    },
+    // 生成图片
+    canvasToUrl(){ 
+      // 在新的画布上绘制
+      const sketch = document.getElementById('sketch');
+      const sketchCxt = sketch.getContext('2d');  
+      let canvasData = cxt.getImageData(scopeX - this.sketchWidth / 2, scopeY - this.sketchHeight / 2, this.sketchWidth, this.sketchHeight);
+      // let canvasData = cxt.getImageData(250, 250, 100, 100);
+      console.log('data', canvasData)
+      sketchCxt.putImageData(canvasData, 0, 0);
+      
+      let url = sketch.toDataURL('image/png', 1); 
+      this.endImage = url;
+    }
   }
 };
 </script>
@@ -65,6 +174,7 @@ padding: 10px 0;
     color: white;
     background: chocolate;
     border: 1px  solid chocolate;
+    cursor: pointer;
 }
 }
 </style>
